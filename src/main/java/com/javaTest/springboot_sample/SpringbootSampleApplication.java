c21508de3e4998ff252f269fa26468c7ccb3d701
Prudhvi Nelaturi <prudhvinelaturi29@gmail.com>
2024-11-18 16:41:15 -0500

    Initial commit

---
+++ src/main/java/com/javaTest/springboot_sample/SpringbootSampleApplication.java
@@ -0,0 +1,16 @@
+package com.javaTest.springboot_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSampleApplication {
+
+	public static void main(String[] args) {
+//		System.out.print("Hello World!");
+		SpringApplication.run(SpringbootSampleApplication.class, args);
+
+
+	}
+
+}
---
+++ src/test/java/com/javaTest/springboot_sample/SpringbootSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaTest.springboot_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
